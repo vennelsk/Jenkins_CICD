1f7674da60392eb94a07d40a8e5a3515b75fa4b7
vennelsk <38183288+vennelsk@users.noreply.github.com>
2022-07-22 22:12:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   secret_key = "V3Uieb3C8Wb9DPVvCId2LO7QKbP2gVcw+fEVzM2A"
 }
 
-resource "aws_instance" "" {
+resource "aws_instance" "appserver" {
   ami = "ami-078a289ddf4b09ae0"
   instance_type = "t2.micro"
 
